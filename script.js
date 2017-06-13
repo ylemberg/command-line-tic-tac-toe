9649331a90fc8c713c38f27320854e1224f90f3f
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-06-12 17:27:00 -0700

    [prog] Finally figured out how to recursively prompt

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-var inquirer = require('inquirer')
+const inquirer = require('inquirer')
 
-console.log('-------')
-console.log('| | | |')
-console.log('-------')
-console.log('| | | |')
-console.log('-------')
-console.log('| | | |')
-console.log('-------')
-inquirer.prompt([{
-  name: 'username',
-  type: 'input',
-  message: 'Player X, please type selected cell, format: (row, col):',
-  validate: value => value.length ? true : 'Please enter your username or e-mail address'
-}]).then(res => {
-  console.log('-------')
-  console.log('| | | |')
-  console.log('-------')
-  console.log('| | | |')
-  console.log('-------')
-  console.log('| | | |')
-  console.log('-------')
-  console.log('res', res)
+const tiles = [[null, null, null], [null, null, null], [null, null, null]]
+
+const prompt = () => {
+  inquirer.prompt([{
+    name: 'username',
+    type: 'input',
+    message: 'Player X, please type selected cell, format: (row, col):'
+  }]).then(res => {
+    console.log('-------')
+    console.log('| | | |')
+    console.log('-------')
+    console.log('| | | |')
+    console.log('-------')
+    console.log('| | | |')
+    console.log('-------')
+    console.log('res', res)
+    prompt()
   }).catch(err => {
     console.log('err', err)
   })
+}
+console.log('-------')
+console.log('| | | |')
+console.log('-------')
+console.log('| | | |')
+console.log('-------')
+console.log('| | | |')
+console.log('-------')
+prompt()
